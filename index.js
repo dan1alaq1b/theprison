a25a9505d156aa150dc506614382b2db0ad6c068
dan1alaq1b <129093729+dan1alaq1b@users.noreply.github.com>
2023-12-26 10:43:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,29 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// ...other middleware and route definitions
-
-// Swagger options
-const options = {
+// Define your Swagger options and paths to your API files (visitor.js, admin.js, etc.)
+const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'The Prison Management System API',
+      title: 'Your API Title',
       version: '1.0.0',
-      description: 'API documentation for The Prison Management System',
+      description: 'Your API Description',
     },
   },
   // Path to files containing OpenAPI specifications (e.g., your routes)
   apis: ['./admin.js', './visitor.js', './generatepass.js', './checkpass.js'],
 };
 
-const specs = swaggerJsdoc(options);
+const specs = swaggerJsdoc(swaggerOptions);
 
 // Serve Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-//app.use('', swaggerUi.serve, swaggerUi.setup(specs)); //access by default
+// Your other middleware and routes can go here...
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
