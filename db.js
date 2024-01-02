8191f422bdd5d4c03b7481bcc002d1cbf94cded7
dan1alaq1b <129093729+dan1alaq1b@users.noreply.github.com>
2024-01-02 21:37:25 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,34 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = 3002;
+app.use(express.json());
 
 // Connection string obtained from MongoDB Atlas
-const uri = 'mongodb+srv://danial:779hRsy0RVMRRRlP@gulag0.ij0pzbn.mongodb.net/?retryWrites=true&w=majority'; // Replace with your connection string
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = 'mongodb+srv://danial:779hRsy0RVMRRRlP@gulag0.ij0pzbn.mongodb.net/?retryWrites=true&w=majority'; // database connection string
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+/*
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const connection = mongoose.connection;
-
+*/
 connection.on('error', console.error.bind(console, 'connection error:'));
 connection.once('open', () => {
   console.log('Connected to MongoDB Atlas for gulagdb');
 });
 
-module.exports = connection; // Export the connection object
+//module.exports = connection; // Export the connection object
+
+client.connect().then(res => {
+  console.log(res);
+});
